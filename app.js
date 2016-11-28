5d31e5a8886ed2fa64a326ad0fd583cf548ba953
Dražen Buljovčić <dbuljovcic@execom.eu>
2016-11-28 22:31:17 +0100

    Pushed back es6 code

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express'),
+const express = require('express'),
       path = require('path'),
       app = express(),
       port = process.env.PORT || 3000,
       router = express.Router();
 
-router.get('/', function(req, res, next){
+router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname + 'src/index.html'));
 });
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'src')));
 
-app.listen(port, function() {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
