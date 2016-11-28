03fa7ee71a76f13a7f9b95e377f49f1af2a79a61
Dražen Buljovčić <dbuljovcic@execom.eu>
2016-11-28 21:23:14 +0100

    Hello world view

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+      path = require('path'),
+      app = express(),
+      port = 3000,
+      router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.sendFile(path.join(__dirname + 'src/index.html'));
+});
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.listen(3000, () => {
+    console.log(`Listening on port ${port}`)
+});
