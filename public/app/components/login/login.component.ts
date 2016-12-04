de8cd4a70b0e3e3016aada553b357900f0bd70c0
Dražen Buljovčić <dbuljovcic@execom.eu>
2016-12-04 16:59:04 +0100

    Merging

--- public/app/components/login/login.component.ts
+++ public/app/components/login/login.component.ts
@@ -21,8 +21,7 @@ export class LoginComponent {
       // To add a validator, we must first convert the string value into an array. The first item in the array is the default value if any, then the next item in the array is the validator. Here we are adding a required validator meaning that the firstName attribute must have a value in it.
       // We can use more than one validator per field. If we want to use more than one validator we have to wrap our array of validators with a Validators.compose function. Here we are using a required, minimum length and maximum length validator.
       'email': [null, Validators.required],
-      'password': [null, Validators.required],
-      'keep-me-logged-in': [false]
+      'password': [null, Validators.required]
     })
   }
 
